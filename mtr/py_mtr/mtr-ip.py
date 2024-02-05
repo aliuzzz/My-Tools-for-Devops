5adca70fa2d3e527064d2dc9a96ff40fce79606c
lzh <roy6152@163.com>
2024-02-05 14:34:16 +0800

    去除md5检测，新增连接池的方法，改善网络不稳定导致的断联

--- mtr/py_mtr/mtr-ip.py
+++ mtr/py_mtr/mtr-ip.py
@@ -1,80 +1,83 @@
-#mtr实时监控终版，一分钟一条，读取mtr_company.csv,产生日志
+import pymysql
+from dbutils.pooled_db import PooledDB
 import os
 import time
-import _thread
-import hashlib
-import logging
+import threading
 import subprocess
-import pandas as pd
 
-# 执行一次任务间隔时间
+from datetime import datetime
 TIME_INTERVAL = 60
-CSV_PATH = "/mtr/mtr_company.csv"
 
-# 读取ip列表
-df = pd.read_csv(CSV_PATH)
-# 每条mtr命令时间间隔
-mtr_interval = TIME_INTERVAL / len(df)
-# 变量初始化
-file_md5_last = 0
+# 创建连接池
+POOL = PooledDB(
+    creator=pymysql,  # 使用要连接的数据库模块
+    maxconnections=100,  # 允许的最大连接数
+    mincached=2,
+    maxcached=5,
+    maxshared=3,
+    blocking=True,  # 连接池中如果没有可用连接后，是否阻塞等待。True等待。
+    maxusage=None,  # 一个连接最多被重复使用的次数，None表示无限制
+    setsession=[],  # 开始会话前执行的命令列表
+    ping=0,
+    host='27.221.49.44',
+    port=3306,
+    user='root',
+    password='QingDaoWx789',
+    database='mtr',
+    charset='utf8'
+)
 
-# 执行mtr命令
+now = datetime.now()
+year = now.year
+month = now.month 
+day = now.day
+hour = now.hour
 def run(ip,year,month,year1,month1,day,region,company,hours,discriptions):
-    result = subprocess.call('echo "------------------------------------------------------------------------------" \
-    >>/data/mtr/%s年%s月/%s年%s月%s日/%s/%s/%s-%s.txt && /usr/local/sbin/mtr -r -i 0.5 -c 60 -n %s >> /data/mtr/%s年%s月/%s年%s月%s日/%s/%s/%s-%s.txt' \
-    %(year,month,year1,month1,day,region,company,hours,discriptions,ip,year,month,year1,month1,day,region,company,hours,discriptions), shell=True)
+    try:
+        file_path = f"/data/mtr/{year}年{month}月/{year}年{month}月{day}日/{region}/{company}/{hours}-{discriptions}.txt"
+        cmd = f'echo "------------------------------------------------------------------------------" >> {file_path}' \
+            f' && /usr/sbin/mtr -r -i 0.5 -c 60 -n {ip} >> {file_path}'
+        result = subprocess.call(cmd, shell=True)
+    except Exception as e:
+        print(f"Error running subprocess: {e}")
 
-# 启动线程
 def my_job():
     year = str(time.strftime('%Y',time.localtime(time.time())))
-    year1 = str(time.strftime('%Y',time.localtime(time.time())))
     month = str(time.strftime('%m',time.localtime(time.time())))
-    month1 = str(time.strftime('%m',time.localtime(time.time())))
     day = str(time.strftime("%d",time.localtime(time.time())))
     hours = str(time.strftime('%H',time.localtime(time.time())))
-    for i in range(len(df)):
-        ip = df.values[i, 1]
-        region = df.values[i, 2]
-        company = df.values[i, 3]
-        discriptions = df.values[i, 4]
-        path = "/data/mtr/"+str(year)+"年"+month+"月/"+year1+"年"+month1+"月"+day+"日/"+region+"/"+company+"/"
-        if os.path.exists(path) == False:
-            os.makedirs(path)
-        _thread.start_new_thread(run, (ip,year,month,year1,month1,day,region,company,hours,discriptions,))
-        
-        # 设定每次mtr后休息时间
-        time.sleep(mtr_interval)
-        logging.basicConfig(
-        filename='/var/log/mtr-ip.log',
-        level=logging.DEBUG,
-        format='[%(asctime)s-%(filename)s-%(levelname)s:%(message)s]'
-        )
-        logging.debug('子线程'+str(i)+'开始执行！')
-        #print(time.strftime('%H:%M:%S', time.localtime(time.time())),'子线程' + str(i) + '已启动')
-    print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())),'一次任务完成')
-    logging.basicConfig(
-        filename='/var/log/mtr-ip.log',
-        level=logging.DEBUG,
-        format='[%(asctime)s-%(filename)s-%(levelname)s:%(message)s]'
-        )
-    logging.debug('一次任务完成！')
 
-# 判断MD5值是否改变
-def md5_changed():
-    with open(CSV_PATH, 'rb') as fp:
-        data = fp.read()
-    file_md5_now = hashlib.md5(data).hexdigest()
-    if file_md5_last != file_md5_now:
-        file_md5_last = file_md5_now
-        #print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())),'修改csv')
-        return True
-    else:
-        return False
+    # 从连接池获取连接
+    conn = POOL.connection()
+    cursor = conn.cursor()
+
+    try:
+        sql_count = "SELECT COUNT(*) FROM mtr_company"
+        sql_mtr_info = "SELECT ip, region, room, description FROM mtr_company"
+        cursor.execute(sql_count)
+        count = cursor.fetchone()[0]
+        mtr_interval = TIME_INTERVAL / count
+        cursor.execute(sql_mtr_info)
+        mtr_info = cursor.fetchall()
+
+        for row in mtr_info:
+            ip = row[0]
+            region = row[1]
+            room = row[2]
+            description = row[3]
+            path = f"/data/mtr/{year}年{month}月/{year}年{month}月{day}日/{region}/{room}/"
+            if not os.path.exists(path):
+                os.makedirs(path)
+            threading.Thread(target=run, args=(ip,year,month,year,month,day,region,room,hours,description)).start()
+            time.sleep(mtr_interval)
+    except pymysql.MySQLError as e:
+        print(f"Database error: {e}")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        cursor.close()
+        conn.close()
 
 if __name__ == '__main__':
     while True:
-        my_job()   
-        if md5_changed:
-            df = pd.read_csv(CSV_PATH)
-
-
+        my_job()
